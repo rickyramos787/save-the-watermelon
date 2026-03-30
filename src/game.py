91f0848d3bc08684a2c499a0134503b98628d8af
rickyramos787 <aramos036@student.sdccd.edu>
2026-03-30 09:07:33 -0700

    Update game.py

--- src/game.py
+++ src/game.py
@@ -3,3 +3,12 @@ game.py
 
 Main game loop for Save the Watermelon.
 """
+
+from src.words import choose_word
+from src.logic import (
+    display_word,
+    is_word_guessed,
+    display_guessed_letters,
+    validate_guess,
+    process_guess,
+)
