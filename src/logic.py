5515757a64764088126d34b146acb47d56dd3697
rickyramos787 <aramos036@student.sdccd.edu>
2026-03-29 22:11:39 -0700

    Update logic.py

--- src/logic.py
+++ src/logic.py
@@ -6,3 +6,12 @@ Contains the core game logic functions for Save the Watermelon.
 
 
 def display_word(secret_word, guessed_letters):
+    """
+    Return the secret word with guessed letters shown
+    and unknown letters displayed as underscores.
+    """
+    output = []
+
+    for letter in secret_word:
+        if letter in guessed_letters:
+            output.append(letter)
