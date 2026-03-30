90cedb669da98c635d8ac375012ed92b44ad84dc
rickyramos787 <aramos036@student.sdccd.edu>
2026-03-29 22:38:47 -0700

    Update logic.py

--- src/logic.py
+++ src/logic.py
@@ -19,3 +19,10 @@ def display_word(secret_word, guessed_letters):
             output.append("_")
 
     return " ".join(output)
+
+
+def is_word_guessed(secret_word, guessed_letters):
+    """
+    Return True if every letter in the secret word
+    has been guessed. Otherwise, return False.
+    """
