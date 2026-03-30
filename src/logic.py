281b7ca58968f9b303875dd5cb985c023e213547
rickyramos787 <aramos036@student.sdccd.edu>
2026-03-29 23:10:53 -0700

    Update logic.py

--- src/logic.py
+++ src/logic.py
@@ -26,3 +26,7 @@ def is_word_guessed(secret_word, guessed_letters):
     Return True if every letter in the secret word
     has been guessed. Otherwise, return False.
     """
+    for letter in secret_word:
+        if letter not in guessed_letters:
+            return False
+    return True
