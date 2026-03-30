fa82363371921e26d3b617d7208bc7ee47a4d8c2
rickyramos787 <aramos036@student.sdccd.edu>
2026-03-29 21:00:00 -0700

    Update words.py

--- src/words.py
+++ src/words.py
@@ -7,3 +7,19 @@ Provides the word list and a function to choose a random word.
 import random
 
 WORD_LIST = [
+    "blanket",
+    "summer",
+    "picnic",
+    "sunshine",
+    "beach",
+    "fruit",
+    "garden",
+    "harvest",
+    "lemonade",
+    "basket",
+    "cantaloupe",
+    "strawberry",
+    "pineapple",
+    "blueberry",
+    "kiwi",
+]
